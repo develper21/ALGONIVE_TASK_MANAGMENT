f24a327878cdbdcb64ccdd12f1dd6b9b22a0bf96
develper21 <githubnarvin@gmail.com>
2025-11-05 20:05:02 +0530

    in userRoutes.js update delete routes and add validatorMiddleware.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middlewares/authMiddleware");
 const { getUsers, getUserById } = require("../controllers/userController");
+const { validateObjectId } = require("../middlewares/validationMiddleware");
 
 const router = express.Router();
 
 router.get("/", protect, adminOnly, getUsers); // get all users (Admin only)
-router.get("/:id", protect, getUserById); // get specific user
-//router.delete("/:id", protect, adminOnly, deleteUser); // delete user (Admin only)
+router.get("/:id", protect, validateObjectId("id"), getUserById); // get specific user
+//router.delete("/:id", protect, adminOnly, validateObjectId("id"), deleteUser); // delete user (Admin only)
 
 module.exports = router;
